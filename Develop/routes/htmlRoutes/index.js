10db7f4e0dccd8ecc5d2b9c4a92555452006d3bd
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-07 11:37:18 -0600

    app get() html routes added

--- Develop/routes/htmlRoutes/index.js
+++ Develop/routes/htmlRoutes/index.js
@@ -1,24 +1,35 @@
-const path = require('path');
-const router = require('express').Router();
+// modified from zookeeper
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+// const path = require('path');
+// const router = require('express').Router();
 
-router.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/animals.html'));
-});
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/notes.html'));
+// });
 
-router.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
-});
+// router.get('/zookeepers', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+// });
 
-router.get('/aquarium', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
-});
+// router.get('/aquarium', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
+// });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
 
-module.exports = router;
+
+// router.get('/api/db', (req, res) => {
+//     let results = notes;
+//     if (req.query) {
+//       results = filterByQuery(req.query, results);
+//     }
+//     res.json(results);
+//   });
+  
+// router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
+
+// module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -14,12 +14,29 @@ const { notes } = require('./db/db.json');
 // app.use(express.json());
 // app.use(express.static('public'));
 
-// Use apiRoutes
+// // Use apiRoutes
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
-app.get('.db.json', (req, res) => {
-  res.send("testing hello!");
+// Function created to filter by id, needed later for removal and re-writing of db.json
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.id) {
+    filteredResults = filteredResults.filter(results => results.id === query.id);
+  }
+  return filteredResults;
+}
+
+app.get('/api/db', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
