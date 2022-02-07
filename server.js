dba8411672982511447a9a5a5fb51f95afff54a7
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-07 14:37:13 -0600

    moved files from subdirectory to root

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { notes } = require('./Develop/db/db.json');
+const { notes } = require('./db/db.json');
 
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 // // Use apiRoutes
 // app.use('/api', apiRoutes);
@@ -45,5 +45,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
-
-//adding to heroku
